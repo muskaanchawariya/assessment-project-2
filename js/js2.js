724906d59f291967133d7f6ca2209243e3ff4553
Muskaan Chawariya <65205357+muskaanchawariya@users.noreply.github.com>
2020-06-11 14:21:06 +0530

    Update js2.js

--- js/js2.js
+++ js/js2.js
@@ -2,7 +2,7 @@ var btn6= document.getElementsByClassName('customer')[0];
 btn6.addEventListener('click',getRatings);
 function getRatings(){
     var xhr= new XMLHttpRequest();
-    xhr.open('GET','/json/ratings.json',true);
+    xhr.open('GET','../json/ratings.json',true);
     xhr.onreadystatechange=function(){
         if(this.readyState==4 && this.status==200){
             var res= JSON.parse(this.responseText);
@@ -32,7 +32,7 @@ var btn7= document.getElementsByClassName('contact')[0];
 btn7.addEventListener('click',getContact);
 function getContact(){
     var xhr=new XMLHttpRequest();
-    xhr.open('GET','/json/contact.json',true);
+    xhr.open('GET','../json/contact.json',true);
     xhr.onreadystatechange=function(){
         if(this.readyState==4 && this.status==200){
             var res=JSON.parse(this.responseText);
